3421ef14eaf6165b5f329c78582d90d0a79480bc
BermanSchultz <berman.schultz@buson.com.br>
2024-05-31 15:49:43 -0300

    feat: create base schemas for common entities

---
+++ apps/api/src/schema/base-schema.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+
+export const userSchema = z.object({
+  id: z.string(),
+  name: z.string().nullable(),
+  email: z.string().email(),
+  createdAt: z.date(),
+})
+
+export const suitabilitySchema = z.object({
+  userId: z.string(),
+  id: z.string(),
+  createdAt: z.date(),
+  score: z.number(),
+});
+
+export const answerSchema = z.object({
+  id: z.string(),
+  questionId: z.string(),
+  choosedAlternatives: z.array(z.number()),
+});
+
+export const paginationSchema = z.object({
+  page: z.number().int().positive().default(1),
+  pageSize: z.number().int().positive().default(10),
+});
+
+export const errorSchema =  z.object({ message : z.string()})
