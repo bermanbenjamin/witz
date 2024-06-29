bdc0eaa01d34c9041ed996ad91823a463c8e9f08
BermanSchultz <berman.schultz@buson.com.br>
2024-06-29 10:31:49 -0300

    fix relative path

--- apps/api/src/models/user.ts
+++ apps/api/src/models/user.ts
@@ -1,5 +1,5 @@
-import type { z } from 'zod'
+import type { z } from "zod";
 
-import type { userSchema } from '@/schemas/base-schema'
+import type { userSchema } from "../schemas/base-schema";
 
-export type UserDTO = z.infer<typeof userSchema>
+export type UserDTO = z.infer<typeof userSchema>;
--- apps/api/src/schemas/base-schema.ts
+++ apps/api/src/schemas/base-schema.ts
@@ -1,12 +1,12 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-import { profileSchema } from '@/models/profile-type'
+import { profileSchema } from "../models/profile-type";
 
 export const suitabilitySchema = z.object({
   id: z.string(),
   createdAt: z.date(),
   score: z.number(),
-})
+});
 
 export const userSchema = z.object({
   id: z.string(),
@@ -19,19 +19,19 @@ export const userSchema = z.object({
   suitabilities: suitabilitySchema.array(),
   role: z.string(),
   createdAt: z.date(),
-})
+});
 
 export const answerSchema = z.object({
   id: z.string(),
   questionId: z.string(),
   choosedAlternatives: z.array(z.number()),
-})
+});
 
 export const paginationSchema = z.object({
   page: z.number().int().positive().default(1),
   limit: z.number().int().positive().default(10),
-})
+});
 
-export const errorSchema = z.object({ message: z.string() })
+export const errorSchema = z.object({ message: z.string() });
 
-export const noContentSchema = z.string().default('No Content')
+export const noContentSchema = z.string().default("No Content");
